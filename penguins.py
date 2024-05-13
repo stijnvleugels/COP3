29b6aa6056a9216b6afc82d036de53e49d22cb61
angelpranger <114067709+angelpranger@users.noreply.github.com>
2024-05-13 22:19:43 +0200

    Basics of simulation finished (force, Brownian equation of motion)

--- penguins.py
+++ penguins.py
@@ -1,24 +1,25 @@
 import numpy as np
 import matplotlib.pyplot as plt
 
-def next_timestep(pos:np.ndarray, vel:np.ndarray, timestep:float, boxsize:float) -> tuple[np.ndarray, np.ndarray, np.array, np.array, float]:
-    ''' Calculate the next position and velocity of the particles given the current position and velocity.
-        - `pos` must be (num_particles, ndim) of positions, 
-        - `vel` must be (num_particles, ndim) of velocities
+def next_timestep(pos:np.ndarray, timestep:float, boxsize:float) -> np.ndarray:
+    ''' Calculate the next position and velocity of the particles given the current position.
+        - `pos` must be (num_particles, ndim) of positions
     '''
-    old_force = force(pos, boxsize)[0]
-    new_pos = (pos + timestep * vel + timestep**2 / 2 * old_force) % boxsize
+    gamma = 1
+    Dt = 3e-5
 
-    new_force, new_nearest_dists = force(new_pos, boxsize)
-    new_vel = vel + timestep / 2 * (new_force + old_force)
+    force = force_field(pos, boxsize)[0]
+    noise = np.random.normal(loc=0, scale=np.sqrt(2*timestep*Dt), size=pos.shape)
 
-    return new_pos, new_vel, new_nearest_dists
+    new_pos = (pos + timestep / gamma * force + noise) % boxsize
 
-def force(pos:np.ndarray, boxsize:float) -> tuple[np.ndarray, np.ndarray, float]:
+    return new_pos
+
+def force_field(pos:np.ndarray, boxsize:float) -> tuple[np.ndarray, np.ndarray, float]:
     ''' Calculate the force on each particle given the current position of all particles.
-        Returns array of shape (num_particles, ndim) for the forces, nearest_dists of shape (num_particles, num_particles) and the pressure.
+        Returns array of shape (num_particles, ndim) for the forces and nearest_dists of shape (num_particles, num_particles).
     '''
-    kappa = 1 ; lamb = 1 ; Top = 1
+    A = 1; kappa = 1 ; lamb = 1 ; optimal_temp = 6
     force = np.zeros((pos.shape[0], pos.shape[1]))
     nearest_dists, diff_vectors = nearest_dist(pos, boxsize)
 
@@ -26,8 +27,8 @@ def force(pos:np.ndarray, boxsize:float) -> tuple[np.ndarray, np.ndarray, float]
     nearest_dists_inf[nearest_dists == 0] = np.inf # has infs for the diagonal (should be in denominator)
     
     for dim in range(force.shape[1]):
-        force[:,dim] = - lamb * (np.sum(np.exp(-kappa*nearest_dists)/nearest_dists_inf, axis=1) - Top) * \
-                        np.sum( - np.exp(-kappa*nearest_dists)*(kappa*nearest_dists + 1)*diff_vectors[:,:,dim] / nearest_dists_inf**3 , axis=1)
+        force[:,dim] = - lamb * (np.sum(A*np.exp(-kappa*nearest_dists)/nearest_dists_inf, axis=1) - optimal_temp) * \
+                        np.sum( -A*np.exp(-kappa*nearest_dists)*(kappa*nearest_dists + 1)*diff_vectors[:,:,dim] / nearest_dists_inf**3 , axis=1)
     return force, nearest_dists
 
 def nearest_dist(pos, boxsize) -> tuple[np.ndarray, np.ndarray]:
@@ -45,18 +46,24 @@ def nearest_dist(pos, boxsize) -> tuple[np.ndarray, np.ndarray]:
     nearest_dists[:,:] = np.linalg.norm(diff_vectors[:,:,:], axis=2)
     return nearest_dists, diff_vectors
 
-
 def main():
-    positions = np.random.rand(10, 2) * 10
-    velocities = np.random.rand(10, 2) * 10
-    timestep = 0.001
+    timestep = 0.0001
+    num_steps = 10000
     boxsize = 10
+    num_particles = 50
+    num_dim = 2
 
-    for i in range(10000):
-        positions, velocities = next_timestep(positions, velocities, timestep=timestep, boxsize=boxsize)[0:2]
+    positions = np.random.rand(num_particles, num_dim) * boxsize
 
-    fig, ax = plt.subplots()
-    ax.scatter(positions[:,0], positions[:,1], color='blue')
+    total_positions = np.zeros([num_steps, num_particles, num_dim])
+    for i in range(num_steps):
+        positions = next_timestep(positions, timestep=timestep, boxsize=boxsize)
+        total_positions[i, :] = positions
+
+    fig, ax = plt.subplots(figsize=(5,5))
+    ax.scatter(total_positions[:,:,0], total_positions[:,:,1], color='red', alpha=0.002)
+    ax.scatter(total_positions[-1,:,0], total_positions[-1,:,1], color='blue')
+    ax.set(xlim=(0,boxsize), ylim=(0,boxsize))
     plt.show()
 
 if __name__ == '__main__':
